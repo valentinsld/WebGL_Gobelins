d515c4dc8433e416f60f2d62c94a8c19cf9389b0
valentinsld <valentin.salaud@gmail.com>
2021-11-04 11:33:20 +0100

    ♻️  Tree: refactor fucntion newBranch -> used object

--- src/js/Tree.js
+++ src/js/Tree.js
@@ -3,17 +3,30 @@ import Seed from './Seed'
 
 const MATERIAL = new THREE.MeshNormalMaterial()
 const MIN_DIFFERENCE_ANGLE = Math.PI*0.2
+const WIDTH = 6
+const HEIGHT = 24
 class Tree {
   constructor({ scene }) {
     Object.assign(this, { scene })
 
-    this.seed = new Seed(2)
+    this.seed = new Seed(4)
 
-    console.log('init')
-    this.newBranch(0, 0, 0, 5, 20, 0, Math.PI * 0.5, this.scene)
+    // this.newBranch(0, 0, 0, WIDTH, HEIGHT, 0, Math.PI * 0.5, this.scene)
+    this.newBranch({
+      pos: {
+        x: 0,
+        y: 0,
+        z: 0,
+      },
+      width: WIDTH,
+      height: HEIGHT,
+      angle: 0,
+      rotationY: Math.PI * 0.5,
+      parentContainer: this.scene
+    })
   }
 
-  newBranch(x, y, z, width, height, angle, rotationY, parentContainer) {
+  newBranch({ pos, width, height, angle, rotationY, parentContainer }) {
     const nextWidth = width * 0.67
 
     // init Mesh
@@ -25,7 +38,7 @@ class Tree {
 
     const parent = new THREE.Object3D()
     parent.add(cylinder)
-    parent.position.set(x, y, 0)
+    parent.position.set(pos.x, pos.y, 0)
     parent.rotation.z = angle
     parent.rotation.y = rotationY
     parentContainer.add(parent)
@@ -33,19 +46,19 @@ class Tree {
 
     if (width < 1) return
 
-    // POSITION :
-    // x = x0 + r*cos(t)
-    // y = y0 + r*sin(t)
-    // const posX = x + height * Math.cos(angle + Math.PI * .5)
-    // const posY = y + height * Math.sin(angle + Math.PI * .5)
-    const posX = 0
-    const posY = height
+    // nextPOSITION :
+    const positionNextBranch = {
+      x: 0,
+      y: height,
+      z: 0
+    }
 
+    // sphere
     const sphere = new THREE.Mesh(
       new THREE.SphereGeometry(nextWidth, 16, 16),
       MATERIAL
     )
-    sphere.position.set(posX, posY, 0)
+    sphere.position.set(positionNextBranch.x, positionNextBranch.y, 0)
     parent.add(sphere)
 
     // const branches = Math.floor(this.seed.nextFloat() * 3)
@@ -53,28 +66,41 @@ class Tree {
 
     if (branches === 0) return
 
-    const rotateY =  rotationY
+    const rotateY = (this.seed.nextFloat() - 0.5) * Math.PI
 
     if (branches === 1) {
-      this.newBranch(posX, posY, 0, nextWidth, height * 0.67, Math.PI * (this.seed.nextFloat() - 0.5), rotateY, parent)
+      // this.newBranch(posX, posY, 0, nextWidth, height * 0.67, Math.PI * (this.seed.nextFloat() - 0.5), rotateY, parent)
     } else if (branches === 2) {
       const angle1 = this.getRandomAngle()
       const angle2 = angle1 < 0 ? 
         Math.max(this.getRandomAngle(), angle1 + MIN_DIFFERENCE_ANGLE)
         : Math.min(this.getRandomAngle(), angle1 - MIN_DIFFERENCE_ANGLE)
 
-      this.newBranch(posX, posY, 0, nextWidth, height * 0.67, angle1, rotateY, parent)
-      this.newBranch(posX, posY, 0, nextWidth, height * 0.67, angle2, rotateY, parent)
+
+      this.newBranch({
+        pos: positionNextBranch,
+        width: nextWidth,
+        height: height * 0.67,
+        angle: angle1,
+        rotationY: rotateY,
+        parentContainer: parent
+      })
+      this.newBranch({
+        pos: positionNextBranch,
+        width: nextWidth,
+        height: height * 0.67,
+        angle: angle2,
+        rotationY: rotateY,
+        parentContainer: parent
+      })
     } else {
 
     }
-
-    // this.newBranch(posX, posY, nextWidth, height * 0.67, Math.PI * (this.seed.nextFloat() - 0.5))
   }
 
   getRandomAngle() {
-    const min = -Math.PI * 0.15
-    const max = Math.PI * 0.15
+    const min = -Math.PI * 0.25
+    const max = Math.PI * 0.25
     return this.seed.nextFloat() * (max - min) + min
   }
 
